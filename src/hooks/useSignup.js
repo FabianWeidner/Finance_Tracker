4eed3c7929e28bd9981d0767d4366a3bfca8d5b7
Fabian Phillipe <fabianphillipe@gmail.com>
2021-11-14 18:43:49 +0100

    build AuthContext, useLogout and Signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -21,6 +23,8 @@ export const useSignup = () => {
       }
 
       await res.user.updateProfile({ displayName });
+
+      dispatch({ type: 'LOGIN', payload: res.user });
 
       setIsPending(false);
       setError(null);
