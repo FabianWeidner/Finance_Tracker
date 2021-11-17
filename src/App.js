0b7e560af4f0c85011deed89e89d5a7b58de61f3
Fabian Phillipe <fabianphillipe@gmail.com>
2021-11-17 17:51:20 +0100

    working on onAuthStateChanged and check if the user is already singed in or not

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // pages & components
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/"
+            element={(!user && <Navigate to="/login" />) || (user && <Home />)}
+          />
 
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/login"
+            element={(user && <Navigate to="/" />) || (!user && <Login />)}
+          />
 
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/signup"
+            element={(user && <Navigate to="/" />) || (!user && <Signup />)}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,37 @@ import './Navbar.scss';
 import { useLogout } from '../hooks/useLogout';
 
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className="navbar">
       <ul>
         <li className="title">myMoney</li>
 
-        <li>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
